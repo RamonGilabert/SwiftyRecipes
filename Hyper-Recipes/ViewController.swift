41bbbec8fafcbc54c69029d70efaa52265943213
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-03 20:50:33 +0200

    Some refactor

--- Hyper-Recipes/ViewController.swift
+++ Hyper-Recipes/ViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UITableViewController
+class ViewController: UIViewController
 {
     let DNHeaderViewHeight: CGFloat = 80.0
     let DNSeparationValueTitle: CGFloat = 15.0
@@ -13,6 +13,7 @@ class ViewController: UITableViewController
     var deviceHeight: CGFloat!
     var headerView: UIView!
     var titleLabel: UILabel!
+    var tableView: UITableView!
 
     // MARK: Initializers
 
@@ -35,19 +36,7 @@ class ViewController: UITableViewController
         deviceWidth = UIScreen.mainScreen().bounds.size.width
         deviceHeight = UIScreen.mainScreen().bounds.size.height
 
-        headerView = UIView(frame: CGRect(x: 0, y: 0, width: deviceWidth, height: DNHeaderViewHeight))
-        headerView.backgroundColor = UIColor(red:0.87, green:0.36, blue:0.5, alpha:1)
-
-        titleLabel = UILabel(frame: CGRect(x: 0, y: DNSeparationValueTitle, width: deviceWidth, height: DNHeaderViewHeight-DNSeparationValueTitle))
-        titleLabel.text = DNTitleString
-        titleLabel.textColor = UIColor.whiteColor()
-        titleLabel.font = UIFont_Recipes.headerFont()
-        titleLabel.textAlignment = NSTextAlignment.Center
-        headerView.addSubview(titleLabel)
-
-        tableView = UITableView(frame: CGRect(x: 0, y: DNHeaderViewHeight, width: deviceWidth, height: deviceHeight - DNHeaderViewHeight))
-
-        view.addSubview(headerView)
+        self.layoutHeaderView()
     }
 
     var dataSource: DATASource!
@@ -62,6 +51,26 @@ class ViewController: UITableViewController
 
             return DATASource(tableView: self.tableView, fetchRequest: request, cellIdentifier: DNCellIdentifier, mainContext: dataStack.mainContext)
         }
+    }
+
+    // MARK: Layout view
+
+    func layoutHeaderView()
+    {
+        headerView = UIView(frame: CGRect(x: 0, y: 0, width: deviceWidth, height: DNHeaderViewHeight))
+        headerView.backgroundColor = UIColor(red:0.87, green:0.36, blue:0.5, alpha:1)
+
+        titleLabel = UILabel(frame: CGRect(x: 0, y: DNSeparationValueTitle, width: deviceWidth, height: DNHeaderViewHeight-DNSeparationValueTitle))
+        titleLabel.text = DNTitleString
+        titleLabel.textColor = UIColor.whiteColor()
+        titleLabel.font = UIFont_Recipes.headerFont()
+        titleLabel.textAlignment = NSTextAlignment.Center
+
+        headerView.addSubview(titleLabel)
+        view.addSubview(headerView)
+
+        tableView = UITableView(frame: CGRect(x: 0, y: DNHeaderViewHeight, width: deviceWidth, height: deviceHeight - DNHeaderViewHeight))
+        view.addSubview(tableView)
     }
 }
 
