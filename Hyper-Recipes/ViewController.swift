30e8ff4c581013f4149a92abfa0a29416e6aa8bf
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-03 19:57:15 +0200

    Add header view

--- Hyper-Recipes/ViewController.swift
+++ Hyper-Recipes/ViewController.swift
@@ -2,7 +2,14 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    let headerViewHeight: CGFloat = 80.0
+
     var dataStack: DATAStack!
+    var deviceWidth: CGFloat!
+    var deviceHeight: CGFloat!
+    var headerView: UIView!
+
+    // MARK: Initializers
 
     init(dataStack: DATAStack)
     {
@@ -14,9 +21,19 @@ class ViewController: UIViewController
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: View timecycle
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
+
+        deviceWidth = UIScreen.mainScreen().bounds.size.width
+        deviceHeight = UIScreen.mainScreen().bounds.size.height
+
+        headerView = UIView(frame: CGRect(x: 0, y: 0, width: deviceWidth, height: headerViewHeight))
+        headerView.backgroundColor = UIColor.whiteColor()
+
+        view.addSubview(headerView)
     }
 }
 
