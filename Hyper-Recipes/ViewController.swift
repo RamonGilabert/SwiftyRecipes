e9bca9c7a025c1d521ec2e23133ebae06b006fe2
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-05 21:25:46 +0200

    Made it to work

--- Hyper-Recipes/ViewController.swift
+++ Hyper-Recipes/ViewController.swift
@@ -6,11 +6,12 @@ let DNSeparationValueTitle: CGFloat = 15.0
 let DNTitleString = "SWIFTY RECIPES"
 let DNCellIdentifier = "CellID"
 
-class ViewController: UICollectionViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
   
   var dataStack: DATAStack!
   var deviceWidth: CGFloat!
   var deviceHeight: CGFloat!
+  var collectionView: UICollectionView!
 
   // MARK: Initializers
 
@@ -31,18 +32,45 @@ class ViewController: UICollectionViewController {
     self.deviceWidth = UIScreen.mainScreen().bounds.size.width
     self.deviceHeight = UIScreen.mainScreen().bounds.size.height
 
-    self.title = DNTitleString
+    layoutCollectionView()
+  }
+
+  // MARK: UICollectionView methods
+
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
+
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    var cell = collectionView.dequeueReusableCellWithReuseIdentifier(DNCellIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+    cell.backgroundColor = UIColor.blackColor()
+    return cell
   }
 
   // MARK: Helper methods
 
   func layoutCollectionView() {
     let collectionViewLayout = UICollectionViewFlowLayout()
-    collectionViewLayout.itemSize = CGSizeMake(50, 50)
+    collectionViewLayout.itemSize = CGSizeMake(self.deviceWidth/2 - 5, self.deviceWidth/2 - 5)
     collectionViewLayout.scrollDirection = UICollectionViewScrollDirection.Vertical
-
-    self.collectionView = UICollectionView(frame: CGRectMake(0, 0, 20, 20), collectionViewLayout: collectionViewLayout)
+    self.collectionView = UICollectionView(frame: CGRectMake(0, DNHeaderViewHeight, self.deviceWidth, self.deviceHeight - DNHeaderViewHeight), collectionViewLayout: collectionViewLayout)
     self.collectionView!.registerClass(UICollectionViewCell.classForCoder(), forCellWithReuseIdentifier: DNCellIdentifier)
+    self.collectionView.backgroundColor = UIColor.whiteColor()
+    self.collectionView.delegate = self
+    self.collectionView.dataSource = self
+
+    let headerView = UIView(frame: CGRectMake(0, 0, self.deviceWidth, DNHeaderViewHeight))
+    headerView.backgroundColor = UIColor(red:0.83, green:0.43, blue:0.36, alpha:1)
+
+    let titleLabel = UILabel(frame: CGRectMake(0, DNSeparationValueTitle, self.deviceWidth, DNHeaderViewHeight - DNSeparationValueTitle))
+    titleLabel.text = DNTitleString
+    titleLabel.font = UIFont_Recipes.headerFont()
+    titleLabel.textAlignment = NSTextAlignment.Center
+    titleLabel.textColor = UIColor.whiteColor()
+
+    headerView.addSubview(titleLabel)
+    self.view.addSubview(headerView)
+    self.view.addSubview(self.collectionView)
   }
 }
 
