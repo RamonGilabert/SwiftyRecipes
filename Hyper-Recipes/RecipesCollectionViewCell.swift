f1c80a33e39e0df4fa67681d907d43fd1ab97f87
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-06 12:13:17 +0200

    Colors in views

--- Hyper-Recipes/RecipesCollectionViewCell.swift
+++ Hyper-Recipes/RecipesCollectionViewCell.swift
@@ -16,9 +16,6 @@ class RecipesCollectionViewCell: UICollectionViewCell {
     let difficultyLabel = UILabel(frame: CGRectMake(DNSeparationLabel, 0, bottomView.frame.size.width - DNSeparationLabel*DNTwoMultiplier, 0))
     let imageView = UIImageView(frame: CGRectMake(0, 0, self.frame.width, self.frame.height - bottomView.frame.height))
 
-    bottomView.backgroundColor = UIColor.blackColor()
-    bottomView.alpha = DNAlphaBottomView
-
     bottomLabel.font = UIFont_Recipes.titleFont()
     bottomLabel.textColor = UIColor.whiteColor()
     bottomLabel.text = recipe.name
@@ -30,6 +27,14 @@ class RecipesCollectionViewCell: UICollectionViewCell {
     difficultyLabel.text = "Difficulty: \(recipe.difficulty)"
     difficultyLabel.sizeToFit()
     difficultyLabel.frame = CGRectMake(DNSeparationLabel, self.frame.height - DNSeparationLabel - difficultyLabel.frame.height, difficultyLabel.frame.width, difficultyLabel.frame.height)
+
+    if recipe.difficulty.integerValue == 1 {
+      bottomView.backgroundColor = UIColor(red:0.6, green:0.79, blue:0.33, alpha:1)
+    } else if recipe.difficulty.integerValue == 2 {
+      bottomView.backgroundColor = UIColor(red:0.96, green:0.43, blue:0.27, alpha:1)
+    } else {
+      bottomView.backgroundColor = UIColor(red:0.93, green:0.38, blue:0.46, alpha:1)
+    }
 
     if recipe.photo.url != nil {
       let urlPhoto = NSURL(string: recipe.photo.url!)
