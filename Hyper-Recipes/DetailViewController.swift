d5fbe3aa6ae037333c517388b864bd494d694db1
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-07 20:35:04 +0200

    Styling

--- Hyper-Recipes/DetailViewController.swift
+++ Hyper-Recipes/DetailViewController.swift
@@ -24,9 +24,25 @@ class DetailViewController: UIViewController {
     self.view.addSubview(headerView)
     self.view.addSubview(self.imageView!)
 
-    let labelTitle = UILabel(frame: CGRectMake(0, 0, 0, 0))
-    labelTitle.font = UIFont_Recipes.headerFont()
+    let labelTitle = UILabel(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width - 40, 0))
+    labelTitle.font = UIFont_Recipes.headerFontRecipes()
     labelTitle.text = self.recipe!.name
+    labelTitle.sizeToFit()
+    labelTitle.frame = CGRectMake(20, self.imageView!.frame.height + 20 + headerView.frame.height, labelTitle.frame.width, labelTitle.frame.height)
+    self.view.addSubview(labelTitle)
+
+    let descriptionRecipe = UILabel(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width - 40, 100))
+    descriptionRecipe.font = UIFont_Recipes.descriptionRecipes()
+
+    if self.recipe!.descriptionID != nil {
+      descriptionRecipe.text = self.recipe!.description
+    } else {
+      descriptionRecipe.text = "There is no description here, but you know, this recipe is "
+    }
+    descriptionRecipe.numberOfLines = 10
+    descriptionRecipe.sizeToFit()
+    descriptionRecipe.frame = CGRectMake(20, labelTitle.frame.origin.y + labelTitle.frame.height + 10, descriptionRecipe.frame.width, descriptionRecipe.frame.height)
+    self.view.addSubview(descriptionRecipe)
   }
 
 }
--- Hyper-Recipes/UIFont+Recipes.swift
+++ Hyper-Recipes/UIFont+Recipes.swift
@@ -6,6 +6,14 @@ class UIFont_Recipes: UIFont
       return UIFont(name: "AvenirNext-DemiBold", size: 22)
   }
 
+  class func headerFontRecipes() -> UIFont! {
+    return UIFont(name: "AvenirNext-DemiBold", size: 26)
+  }
+
+  class func descriptionRecipes() -> UIFont! {
+    return UIFont(name: "AvenirNext-Medium", size: 18)
+  }
+
   class func titleFont() -> UIFont! {
     return UIFont(name: "AvenirNextCondensed-DemiBold", size: 18)
   }
